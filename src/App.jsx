dadf38d135342239ea0fac44133173cc9f6276b2
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2024-12-06 23:33:05 +0100

    completato esercizio

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
 
 function App() {
   const IngredientsList = ['zucchero', 'farina', 'uova', 'lievito'];
@@ -10,12 +12,15 @@ function App() {
     published: false,
   });
   const [articles, setArticles] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [articleToDelete, setArticleToDelete] = useState(null);
+  const API_URL = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
-    fetch('http://localhost:3000/posts')
+    fetch(`${API_URL}/posts`)
       .then(res => res.json())
       .then(data => setArticles(data));
-  }, []);
+  }, [API_URL]);
 
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -42,14 +47,14 @@ function App() {
       return;
     }
     
-    fetch('http://localhost:3000/posts', {
+    fetch(`${API_URL}/posts`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(formData)
     })
-      .then(response => response.json())
+      .then(res => res.json())
       .then(data => {
         setArticles([...articles, data]);
         setFormData({
@@ -60,20 +65,23 @@ function App() {
           published: false,
         });
       })
-      .catch(error => console.error('Errore nell\'invio dei dati:', error));
-  };
+    };
 
-  const deleteArticle = (articleIndex) => {
-    const articleToDelete = articles[articleIndex];
-    fetch(`http://localhost:3000/posts/${articleToDelete.id}`, {
+  const handleDelete = () => {
+    fetch(`${API_URL}/posts/${articleToDelete.id}`, {
       method: 'DELETE'
     })
       .then(() => {
-        const newArticles = [...articles];
-        newArticles.splice(articleIndex, 1);
+        const newArticles = articles.filter(article => article.id !== articleToDelete.id);
         setArticles(newArticles);
+        setShowModal(false);
       })
-     };
+    };
+
+  const confirmDelete = (article) => {
+    setArticleToDelete(article);
+    setShowModal(true);
+  };
 
   const editArticle = (articleIndex) => {
     const newName = prompt('Modifica il nome:', articles[articleIndex].name);
@@ -88,65 +96,64 @@ function App() {
   return (
     <>
     <div className="container">
-     <h1>Crea nuovo dolce</h1>
+    <h2>Crea nuovo dolce</h2>
     <form onSubmit={handleFormSubmit} className="row g-3">
           
     <div className="col-3">
     <label htmlFor="name" className="form-label">
-       Nome
+      Nome
     </label>
-    <input
-    type="text"
-    className="form-control"
-    name="name"
-    value={formData.name}
-    onChange={handleInputChange}
-    />
+      <input
+      type="text"
+      className="form-control"
+       name="name"
+      value={formData.name}
+      onChange={handleInputChange}
+      />
     </div>
     <div className="col-3">
-    <label htmlFor="description">
-      Descrizione
-    </label>
-     <textarea
-    type="text"
-    className="form-control"
-    name="description"
-    value={formData.description}
-    onChange={handleInputChange}></textarea>
-     </div>
-
-    <div className="col-3">
-    <label htmlFor="image" className="form-label">
-      Immagine
-    </label>
-    <input
-      type="text"
+     <label htmlFor="description">
+       Descrizione
+      </label>
+       <textarea
+       type="text"
+       className="form-control"
+       name="description"
+       value={formData.description}
+       onChange={handleInputChange}></textarea>
+      </div>
+      <div className="col-3">
+      <label htmlFor="image" className="form-label">
+         Immagine
+      </label>
+       <input
+       type="text"
       className="form-control"
       name="image"
       value={formData.image}
-      onChange={handleInputChange}
-    />
-    </div>
-      <div className="mb-2">
+       onChange={handleInputChange}
+        />
+      </div>
+      <div className="m-2">
       <label htmlFor="name" className="form-label">
          Ingredienti:
-     </label>
-      {IngredientsList.map(tag => (
-      <div key={tag} className="form-check">
-       <input
-        type="checkbox"
-        name="ingredients"
-        value={tag}
-       checked={formData.ingredients.includes(tag)}
-        onChange={handleInputChange}
-        className="form-check-input"
-        />
-        <label className="form-check-label">{tag}</label>
-        </div>
+       </label>
+       {IngredientsList.map(tag => (
+        <div key={tag} className="form-check">
+         <input
+          type="checkbox"
+           name="ingredients"
+          value={tag}
+          checked={formData.ingredients.includes(tag)}
+           onChange={handleInputChange}
+           className="form-check-input"
+          />
+         <label className="form-check-label">{tag}</label>
+         </div>
         ))}
-       </div>
+        </div>
           
-       <div className ="col-3">
+       <div className ="form-check ">
          <input type="checkbox" 
          name="published"
           checked={formData.published}
@@ -157,35 +164,46 @@ function App() {
               Pubblica
               </label>
             </div>
-       
-      
-        <div className="col-3">
-          <button type="submit" className="btn btn-primary">Aggiungi Articolo</button>
+       <div className=" col-3 ">
+          <button type="submit" className="btn btn-primary"> Modifica</button>
           </div>
+          
         </form>
         <hr/>
-        
-        <div className="container">
-          < h1 className="mt-5">Dolci e Salato</h1>
-           {articles.map((article, articleIndex) => (
-            <div key={articleIndex} className="card" >
-              {article.image && <img src={`http://localhost:3000${article.image}`} className="card-img-top" alt={article.name} />}
-              <div className="card-body">
-             <h5 className="card-title">{article.name}</h5>
-              <p className="card-text">{article.description}</p>
-              <p className="card-text">{article.published}</p>
-              <p className="card-text">Ingredienti: {article.ingredients.join(', ')}</p>
-              <div className="button-container">
-              <button className="btn btn-warning btn-sm me-2" onClick={() => editArticle(articleIndex)}>Modifica</button>
-              <button className="btn btn-danger btn-sm" onClick={() => deleteArticle(articleIndex)}><span className="material-symbols-outlined">delete</span></button>
-             </div>
-           </div>
-         </div>
-        ))}
+        <div className="card-container row g-3 ">
+          <h1 className="mt-5">Dolci e Salato</h1>
+          {articles.map((article, articleIndex) => (
+          <div key={articleIndex} className="card">
+          {article.image && <img src={`${API_URL}${article.image}`} className="card-img" alt={article.name} />}
+          <div className="card-body">
+           <h5 className="card-title">{article.name}</h5>
+            <p className="card-text">{article.description}</p>
+            <p className="card-text">{article.published}</p>
+            <p className="card-text">Ingredienti: {article.ingredients.join(', ')}</p>
+            <div className="button-container">
+            <button className="btn btn-danger btn-sm" onClick={() => confirmDelete(article)}><span className="delete">Elimina</span></button>
+            </div>
+          </div>
+          </div>
+          ))}
+        </div>
       </div>
-    </div>
-   </>
- );
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Conferma eliminazione</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Sei sicuro di voler eliminare?</Modal.Body>
+        <Modal.Footer>
+        <Button variant="secondary" onClick={() => setShowModal(false)}>
+           Annulla
+        </Button>
+        <Button variant="danger" onClick={handleDelete}>
+          Elimina
+        </Button>
+      </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default App;
